c4a1a5664f7a10c4503cf2b070339d254147b496
Matt <matt@incitefulgames.com>
2017-05-04 23:21:25 -0400

    add row to graph

--- main/Graph.java
+++ main/Graph.java
@@ -1,7 +1,24 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Matt on 5/4/2017.
  */
 public class Graph {
+    private List<List<Integer>> graph = new ArrayList<>();
+
+    public void addRow(String row){
+        String[] parts = row.split(" ");
+        ArrayList<Integer> integers = new ArrayList<>();
+        for(String s : parts){
+             integers.add( Integer.valueOf(s) );
+        }
+        graph.add(integers);
+    }
+
+    public List<Integer> getRow(int i){
+        return graph.get(i);
+    }
 }
--- test/GraphTest.java
+++ test/GraphTest.java
@@ -1,7 +1,26 @@
 package test;
 
+import main.Graph;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
 /**
  * Created by Matt on 5/4/2017.
  */
 public class GraphTest {
+
+    @Test
+    public void addRowToGraphTest(){
+        Graph subject = new Graph();
+
+        subject.addRow("1 2 3 4 5");
+
+        assertEquals(1, subject.getRow(0).get(0).intValue());
+        assertEquals(2, subject.getRow(0).get(1).intValue());
+        assertEquals(3, subject.getRow(0).get(2).intValue());
+        assertEquals(4, subject.getRow(0).get(3).intValue());
+        assertEquals(5, subject.getRow(0).get(4).intValue());
+    }
 }
